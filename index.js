f97a5c3fbff292956ba3c520ec4718f82798a1dd
OliaOrel <olia.orel13@gmail.com>
2019-06-12 17:46:28 +0300

    Start new bot

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('OMG');
+const TelegramBot = require('node-telegram-bot-api');
+const token = '861338210:AAGkophdlKbg7BJkoPy5EFyEETAQLUTYgOk';
+// const BOT_TOKEN = process.env.GB_STATUS_BOT_TOKEN;
+const bot = new TelegramBot(token,{polling:true});
+// const bot = new TelegramBot(BOT_TOKEN, { polling: true });
+
+bot.onText(/\/work/, msg => {
+    bot.sendMessage(msg.chat.id, 'works');
+});
